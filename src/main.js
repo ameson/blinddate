dbf768cc936c2e3a4a14814177622c2441eeef12
ameson <xvws321@qq.com>
2024-11-25 13:39:40 +0800

    Update main.js
    更新id

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import 'vant/lib/index.css'
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?YOUR_TRACKING_ID"; // 请替换为你的百度统计跟踪ID
+  hm.src = "https://hm.baidu.com/hm.js?a2e1dda791e37f17ddffc039aca159ec"; // 请替换为你的百度统计跟踪ID
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
