bf471b33e460042d3f88e09e65e02ae7593c6c8b
via <blinddate@temp.now>
2024-11-25 13:38:27 +0800

    feat: 添加百度统计代码

--- src/main.js
+++ src/main.js
@@ -7,6 +7,15 @@ import store from './store'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 
+// 百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?YOUR_TRACKING_ID"; // 请替换为你的百度统计跟踪ID
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 // 创建应用实例
 const app = createApp(App)
 
@@ -17,3 +26,10 @@ app.use(Vant)
 
 // 挂载应用
 app.mount('#app')
+
+// 路由切换时进行百度统计
+router.afterEach((to) => {
+  if (typeof _hmt !== 'undefined') {
+    _hmt.push(['_trackPageview', to.fullPath]);
+  }
+})
